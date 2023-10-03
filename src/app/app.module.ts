c5acd211d8c965d83f45853f479cea11d9a87212
İnan güçlü <77547694+inanguclu@users.noreply.github.com>
2023-10-03 22:53:36 +0300

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
+import { ValidateDirective } from './directives/validate.directive';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ValidateDirective
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      closeButton:true,
+      preventDuplicates:true,
+      timeOut:3000,
+      progressBar:true,
+      positionClass:"'toast-bottom-right'"
+    }),
     FormsModule
   ],
   providers: [],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -7,21 +8,81 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
-  userNameOrEmail:string = "";
-  password:string="";
-
-  signIn(form:NgForm){
-    if(this.userNameOrEmail===""){
-      alert("username or email must be valid");
-      return;
-    }
-    if(this.password===""){
-      alert("password must be valid");
-      return;
-    }
-    console.log(this.userNameOrEmail);
-    console.log(this.password);
+  isShowPassword: boolean = false;
+  isPasswordFocus: boolean = false;
+  constructor(private toastr:ToastrService){
     
   }
+
+
+
+  signIn(form: NgForm) {
+    if (form.valid) {
+      console.log(form);
+      console.log(form.value);
+    }
+      
+    
+  }
+
+  showOrHidePassword(password: HTMLInputElement) {
+    if (this.isShowPassword) {
+      this.isShowPassword = false;
+
+      password.type = "password";
+    } else {
+      this.isShowPassword = true;
+
+      password.type = "text";
+    }
+
+  }
+
+  checkRegexPatternForPassword(el: HTMLInputElement) {
+    const text = el.value;
+
+    //buyuk harf kontrol
+    const upperCaseRegex = /[A-Z]/;
+    const upperCaseResult = upperCaseRegex.test(text);
+    const upperLetterEl = document.getElementById("upperLetter");
+    upperLetterEl?.classList.add(upperCaseResult ? 'pw-success' : 'pw-error');
+    upperLetterEl?.classList.remove(!upperCaseResult ? 'pw-success' : 'pw-error');
+
+    //kucuk harf kontrol
+    const lowerCaseRegex = /[a-z]/;
+    const lowerCaseResult = lowerCaseRegex.test(text);
+    const lowerLetterEl = document.getElementById("lowerLetter");
+    lowerLetterEl?.classList.add(lowerCaseResult ? 'pw-success' : 'pw-error');
+    lowerLetterEl?.classList.remove(!lowerCaseResult ? 'pw-success' : 'pw-error');
+
+    //özel harf kontrol
+    const specialCaseRegex = /[!@#$%^&*()_+\-=\[\]{};:'\\|,.<>\/?]+/;
+    const specialCaseResult = specialCaseRegex.test(text);
+    const specialLetterEl = document.getElementById("specialLetter");
+    specialLetterEl?.classList.add(specialCaseResult ? 'pw-success' : 'pw-error');
+    specialLetterEl?.classList.remove(!specialCaseResult ? 'pw-success' : 'pw-error');
+
+    //numeratic harf kontrol
+    const numeraticCaseRegex = /[0-9]/;
+    const numeraticCaseResult = numeraticCaseRegex.test(text);
+    const numeraticLetterEl = document.getElementById("numeraticLetter");
+    numeraticLetterEl?.classList.add(numeraticCaseResult ? 'pw-success' : 'pw-error');
+    numeraticLetterEl?.classList.remove(!numeraticCaseResult ? 'pw-success' : 'pw-error');
+
+
+
+    //MİNİMUM 6 KARAKTER kontrol
+    const minSixCharacterEl=document.getElementById("minSixCharacter");
+    if(text.length<6){
+      minSixCharacterEl?.classList.add("pw-error");
+      minSixCharacterEl?.classList.remove("pw-success");
+    }else {
+      minSixCharacterEl?.classList.remove("pw-error");
+      minSixCharacterEl?.classList.add("pw-success");
+    }
+
+  }
+
+
+
 }
